45e0fe2ee994d1e6d8bf3be0c4ea09526028609f
sveet <sveetlana2000@mail.ru>
2023-02-01 12:35:50 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+/**
+ * @author sveet
+ * @date 01.02.2023
+ */
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Svetlana");
+        System.out.println("17.03.2000");
+    }
+}
