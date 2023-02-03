232c7f3040e2cdcef9958777dfd08e5c05b832ad
sveet <sveetlana2000@mail.ru>
2023-02-03 13:42:08 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+/**
+ * @author sveet
+ * @date 03.02.2023
+ */
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie.";
+        int year = 2023;
+        idea += year;
+        System.out.println(idea);
+    }
+}
