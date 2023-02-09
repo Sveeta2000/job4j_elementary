7f6a0d6f6b57d5e728a20fb1dad9e497fb996dc8
sveet <sveetlana2000@mail.ru>
2023-02-09 18:40:21 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+/**
+ * @author sveet
+ * @date 09.02.2023
+ */
+public class Max {
+
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+}
