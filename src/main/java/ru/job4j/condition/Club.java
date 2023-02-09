57566d7e866a214c7f03329a1936d37999ef3ddb
sveet <sveetlana2000@mail.ru>
2023-02-09 19:14:26 +0300

    4.3.2. Сокращенный оператор || [#333784]

---
+++ src/main/java/ru/job4j/condition/Club.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author sveet
+ * @date 09.02.2023
+ */
+public class Club {
+
+    public static void permission(boolean hasMoney, boolean beFriend) {
+        if (hasMoney || beFriend) {
+            System.out.println("I can go to the club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
