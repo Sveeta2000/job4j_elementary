8e9075dda8dac3cab34d4004ce39c6bfa8c39521
sveet <sveetlana2000@mail.ru>
2023-02-10 17:39:03 +0300

    4.5. Отладка программы в IDEA [#159464]

---
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+/**
+ * @author sveet
+ * @date 10.02.2023
+ */
+public class ThreeMax {
+
+    public static int max(int first, int second, int third) {
+        int result = third;
+        if (first > second && first >= third) {
+            result = first;
+        }
+        if (second >= first && second >= third) {
+            result = second;
+        }
+        return result;
+    }
+}
