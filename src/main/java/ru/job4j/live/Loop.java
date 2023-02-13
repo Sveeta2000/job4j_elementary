ad3c169fe3f6fb79d54eee2a17179bc593e85292
sveet <sveetlana2000@mail.ru>
2023-02-13 20:45:26 +0300

    6.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,14 @@
+package ru.job4j.live;
+
+/**
+ * @author sveet
+ * @date 13.02.2023
+ */
+public class Loop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
