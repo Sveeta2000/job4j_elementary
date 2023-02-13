bee7bcfc7bbbab60614102f5b85827c7e90fc510
sveet <sveetlana2000@mail.ru>
2023-02-13 13:31:38 +0300

    5.0. Цикл for. Определение. [#3572]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+/**
+ * @author sveet
+ * @date 13.02.2023
+ */
+public class LoopFor {
+
+    public static void main(String[] args) {
+        for (int index = 5; index <= 10; index++) {
+            System.out.println(index);
+        }
+    }
+}
