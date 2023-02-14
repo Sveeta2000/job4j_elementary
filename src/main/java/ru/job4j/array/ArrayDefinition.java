d66fce5c1878848ef4b63e400d362a206f71a9ba
sveet <sveetlana2000@mail.ru>
2023-02-14 12:39:56 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+/**
+ * @author sveet
+ * @date 14.02.2023
+ */
+public class ArrayDefinition {
+
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
