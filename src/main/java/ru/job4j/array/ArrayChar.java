a74f3947bf54ce539e0effb20127e5043d9f2e5b
sveet <sveetlana2000@mail.ru>
2023-02-15 19:02:04 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * @author sveet
+ * @date 15.02.2023
+ */
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != word[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author sveet
+ * @date 15.02.2023
+ */
+class ArrayCharTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result).isFalse();
+    }
+}
