90ab92c0ef35699095805c46e5035b34f6ff25e9
sveet <sveetlana2000@mail.ru>
2023-02-15 20:01:41 +0300

    6.4.3. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * @author sveet
+ * @date 15.02.2023
+ */
+public class EndsWith {
+
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
